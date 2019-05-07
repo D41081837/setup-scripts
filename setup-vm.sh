f4a12596fbfc9c33f7f5a7ec4d82436ee95201c9
D41081837 <robert.shell@adtalem.com>
2019-05-07 13:36:27 -0500

    move vm destroyers back into if statement

--- setup-vm.sh
+++ setup-vm.sh
@@ -6,9 +6,6 @@ VM_NAME=adtalem.local
 # Turn off the VMs.
 cd ~/vms/CMS-Drupal-MKTG/
 vagrant halt 2> /dev/null
-vboxmanage startvm "$VM_NAME" --type emergencystop 2> /dev/null
-vboxmanage unregistervm "$VM_NAME" --delete 2> /dev/null
-vagrant destroy -f 2> /dev/null
 
 # Check for existing VM files.
 
@@ -19,7 +16,9 @@ if [ ! -e "$VM_NAME" ]; then
   vagrant ssh
 else
   # Remove existing VM files.
-
+  vboxmanage startvm "$VM_NAME" --type emergencystop 2> /dev/null
+  vboxmanage unregistervm "$VM_NAME" --delete 2> /dev/null
+  vagrant destroy -f 2> /dev/null
   cd ~/VirtualBox\ VMs
   rm -rf "$VM_NAME" 2> /dev/null
 
