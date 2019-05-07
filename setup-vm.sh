564df20d4b9b0b99877bd1e57e260893b45ebee7
D41081837 <robert.shell@adtalem.com>
2019-05-07 12:10:17 -0500

    VM changes

--- setup-vm.sh
+++ setup-vm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # Set the VM name.
 VM_NAME=adtalem.local
@@ -19,10 +18,12 @@ if [ ! -e "$VM_NAME" ]; then
   vagrant ssh
 else
   # Remove existing VM files.
-  rm -rf VM_NAME
+  vboxmanage startvm "$VM_NAME" --type emergencystop 2> /dev/null
+  vagrant halt 2> /dev/null
+  vagrant destroy -f 2> /dev/null
+  vboxmanage unregistervm "$VM_NAME" --delete 2> /dev/null
+  rm -rf "$VM_NAME" 2> /dev/null
 
-  vboxmanage unregistervm adtalem.local --delete
-  
   # Build the VM.
   cd ~/vms/CMS-Drupal-MKTG/
   vagrant up
